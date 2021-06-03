559d2b15a18d0171d82e2f41ee199e378e2e1c82
Hoan19 <85276961+Hoan19@users.noreply.github.com>
2021-06-03 15:11:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-    introTitle: 'Em à!',
+    introTitle: 'EM à!',
     introDesc: `Gặp em anh bỗng muốn thờ.
     Không "thờ ích thích" mà "thờ ương thương".
     Câu thả thính là anh coppy đó,
     nhưng tình cảm anh dành cho em là thật lòng.
     Đằng sau chữ thích là chữ thương`,
-    btnIntro: 'thích',
+    btnIntro: 'Thích',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
@@ -13,7 +13,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều lém lém 😘😘',
     messDesc: 'Em ơi, mẹ anh gọi em là con dâu kìa!',
     btnAccept: 'Okiiiii lun <3',
 }
