a251ea150980c928b3d114eb9ceec7a5cb0ed26f
Hoan19 <85276961+Hoan19@users.noreply.github.com>
2021-06-03 15:14:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const CONFIG = {
-    introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'EM à!',
+    introDesc: `Gặp em anh bỗng muốn thờ.
+    Không "thờ ích thích" mà "thờ ương thương".
+    Câu thả thính là anh coppy đó,
+    nhưng tình cảm anh dành cho em là thật lòng.
+    Đằng sau chữ thích là chữ thương`,
+    btnIntro: 'Thích',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
@@ -12,8 +13,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều lém lém 😘😘',
+    messDesc: 'Em ơi, mẹ anh gọi em là con dâu kìa!',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
